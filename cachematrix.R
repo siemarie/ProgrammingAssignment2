f6afc497c9cf92efd7501715358c50e55cd69304
siemarie <jessie.hand@gmail.com>
2014-04-27 20:20:08 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following script checks the cache for an existing inversed matrix given x.
+## If found, the script returns the cached matrix. Otherwise, the script creates
+## a new inversed matrix and stores it in the cache.
 
-## Write a short comment describing this function
+storedX<<-NULL
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  set <<- function(y) {
+    x <<- y
+    inv_x <<- NULL
+  }
+  get <- function() x
+  setinverse <<- function(inverse) inv_x <<-inverse
+  getinverse <<- function() inv_x
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(!is.null(storedX)) {
+    if(identical(x,storedX)) {
+      if (!is.null(inv_x)) {
+        message("Retrieving inversed matrix from cache.")
+        inv_x<<-inv_x
+      } else {
+        set(x)
+        storedX<<-x
+        message("Inversed matrix not found in cache. Creating a new inversed matrix.")
+        require("MASS")
+        library("MASS")
+        makeCacheMatrix(x)
+        inv_x <<- ginv(x)
+        setinverse(inv_x)
+      }
+    } else {
+      set(x)
+      storedX<<-x
+      message("Inversed matrix not found in cache. Creating a new inversed matrix.")
+      require("MASS")
+      library("MASS")
+      makeCacheMatrix(x)
+      inv_x <<- ginv(x)
+      setinverse(inv_x)
+    }
+  } else {
+    set(x)
+    storedX<<-x
+    message("Inversed matrix not found in cache. Creating a new inversed matrix.")
+    require("MASS")
+    library("MASS")
+    makeCacheMatrix(x)
+    inv_x <<- ginv(x)
+    setinverse(inv_x)
+  }
+  inv_x
 }
